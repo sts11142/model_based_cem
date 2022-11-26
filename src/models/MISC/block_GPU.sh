c31bee953cba718614d42ebf641096019184f504
Shota Sato <last11142@gmail.com>
2022-11-26 21:32:44 +0900

    [delete] MISC --cached
    gitが重い原因？

--- src/models/MISC/block_GPU.sh
+++
@@ -1,4 +0,0 @@
-CUDA_VISIBLE_DEVICES=2 nohup python block_GPU.py >block.log 2>&1 &
-# CUDA_VISIBLE_DEVICES=5 nohup python block_GPU.py >block.log 2>&1 &
-# CUDA_VISIBLE_DEVICES=6 nohup python block_GPU.py >block.log 2>&1 &
-# CUDA_VISIBLE_DEVICES=7 nohup python block_GPU.py >block.log 2>&1 &
